64d53a4dd19fdbb33b4dfb08482e28dbb2e2b022
Camilla Sancricca <48389187+camillasancricca@users.noreply.github.com>
2023-10-02 10:53:19 +0200

    Add files via upload

--- fdtool.py
+++ fdtool.py
@@ -39,30 +39,9 @@ def main(filePath):
 
     if not fileExtension == "pkl":
         # Read in file, determine whether a pkl or txt/csv
-        try:
-            
-            # Detect delimiter
-            sniffer = csv.Sniffer()
-            sniffer.preferred=[',','|',';',':','~']            
-            csvFile=open(filePath, 'r')
-            for row in csv.reader(csvFile,delimiter="\t"):
-                row1=row
-                break;
-            csvFile.close()
-            dialect=sniffer.sniff(str(row1))
-            sepType = dialect.delimiter
+        # Read in pandas data frame from csv file
+        df = pd.read_csv(filePath, sep = ',');
 
-            if sepType not in {",", "|", ";", ":", "~"}:
-                print("Invalid delimiter")
-                sys.stdout.flush()
-                return;
-
-            # Read in pandas data frame from csv file
-            df = pd.read_csv(filePath, sep = sepType);
-        except IOError:
-            print("File not found")
-            sys.stdout.flush()
-            return;
     else:
         try:
             # Read in pandas data fram pkl file
